9004a54f9bce759f33e512f63abb9d9c71c6f1ab
Taylor Carpenter <taylor@codecafe.com>
2012-09-25 23:52:10 -0500

    json working

--- src/app/web.clj
+++ src/app/web.clj
@@ -12,24 +12,15 @@
     (= "pathname" "/.json") true
     :else false)))
 
-  ;(let [mybody (to_json_maybe remote-addr)])
-      ;(= (json? req) true) (to_json remote-addr)
 (defn app [req]
   (let [remote-addr (ip req)]
-;  (let [mybody (cond
-;      (= (json? req) true) (str "ip:" remote-addr)
-;      :else remote-addr))
-;   ( let [mybody (str "ip: " remote-addr)]
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   ;:body mybody
    :body (cond
-      (= (json? req) true) (str "ip:" remote-addr)
+      (= (json? req) true) (format "{\"ip\": \"%s\"}" remote-addr)
        :else remote-addr)
   }))
-
 
 (defn -main []
   (let [port (Integer/parseInt (get (System/getenv) "PORT" "5000"))]
     (run-jetty app {:port port})))
-
