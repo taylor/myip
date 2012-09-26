a7ba60144dd2742e2a827ef16f9f9b0b6892d3c3
Taylor Carpenter <taylor@codecafe.com>
2012-09-25 19:14:21 -0500

    remote addr req... port fun

--- src/app/web.clj
+++ src/app/web.clj
@@ -1,11 +1,19 @@
-(ns demo.web
+(ns app.web
   (:use ring.adapter.jetty))
+
+(defn ip [request]
+  (or ((:headers request) "x-forwarded-for")
+      (:remote-addr request)))
+
+;(let [remote_ip (ip req))]
 
 (defn app [req]
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body "Hello from Clojure!\n"})
+   :body "test"})
+;   :body "ip:" + ip {req}})
 
+;  (let [port (Integer/parseInt (System/getenv "PORT"))]
 (defn -main []
   (let [port (Integer/parseInt (System/getenv "PORT"))]
     (run-jetty app {:port port})))
